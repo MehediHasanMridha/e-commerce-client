1a268396a825607340031917e70dd41420f6abbc
Anamul029 <anamhaque.5101@gmail.com>
2024-11-06 20:40:37 +0600

    adding style

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,8 +1,8 @@
 
 const Root = () => {
     return (
-        <div className="bg-yellow-400 min-h-screen">
-            <h1 className="text-center font-bold py-20 text-white">Hello i am from root component and the project is initialized successfully.</h1>
+        <div className="bg-[#af6767] min-h-screen">
+            <h1 className="text-center font-bold py-20 text-white">Hello i am from root component and the project was initialized successfully.</h1>
         </div>
     );
 };
