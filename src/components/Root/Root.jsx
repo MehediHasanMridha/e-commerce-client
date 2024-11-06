4cd14a31a41407a50f805b0b8e7903c09b303283
jobaer6768 <jobaer9898@gmail.com>
2024-11-07 00:29:26 +0600

    just checking

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,10 +1,13 @@
-
 const Root = () => {
-    return (
-        <div className="bg-[#af6767] min-h-screen">
-            <h1 className="text-center font-bold py-20 text-white">Hello i am from root component and the project was initialized successfully.</h1>
-        </div>
-    );
+  return (
+    <div className="bg-[#af6767] min-h-screen">
+      <h1 className="text-center font-bold py-20 text-white">
+        Hello i am from root component and the project was initialized
+        successfully.
+      </h1>
+      <h3>Joaber</h3>
+    </div>
+  );
 };
 
 export default Root;
