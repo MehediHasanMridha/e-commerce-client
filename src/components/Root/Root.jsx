181b10edf43dce17a04fbb5c0a7ca2b961839f28
Anamul029 <anamhaque.5101@gmail.com>
2024-11-07 00:28:53 +0600

    add navBar

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
 
 const Root = () => {
     return (
-        <div className="bg-[#af6767] min-h-screen">
+        <div className="bg-[#fffbfb] min-h-screen">
+            <NavBar></NavBar>
+            <Outlet></Outlet>
             <h1 className="text-center font-bold py-20 text-white">Hello i am from root component and the project was initialized successfully.</h1>
         </div>
     );
