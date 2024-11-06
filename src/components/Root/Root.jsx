779e82bd3e15fa37722f858fd62af7c41dfce11f
Anamul029 <anamhaque.5101@gmail.com>
2024-11-06 17:01:23 +0600

    first commit

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,10 @@
+
+const Root = () => {
+    return (
+        <div className="bg-yellow-400 min-h-screen">
+            <h1 className="text-center font-bold py-20 text-white">Hello i am from root component and the project is initialized successfully.</h1>
+        </div>
+    );
+};
+
+export default Root;
