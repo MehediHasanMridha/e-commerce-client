493e21d02ae24753fc28018bd7adbb5ed0c805fd
jobaer6768 <jobaer9898@gmail.com>
2024-11-07 21:18:19 +0600

    new branch created and deleted old files

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,13 +1,5 @@
-import HotDeal from "../hot-deal";
-import ProductCategory from "../product-category";
-
 const Root = () => {
-  return (
-    <div className="">
-      <ProductCategory />
-      <HotDeal />
-    </div>
-  );
+  return <div className="">Shop Branch</div>;
 };
 
 export default Root;
