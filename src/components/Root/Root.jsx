fe013a58bd2c56049ff25372916af88d7096cfcf
Anamul029 <anamhaque.5101@gmail.com>
2024-11-08 21:59:01 +0600

    create order-datails branch

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,70 +1,7 @@
 const Root = () => {
-  return <div className="bg-[#f8f8f8] min-h-screen lg:w-[80%] lg:mx-auto w-full">
-    <h2 className="mx-3 text-lg text-[#0a83eb] font-semibold my-3">Categorys</h2> <hr />
-
-    <div className=" md:mx-3 mx-3 my-3 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-4 md:gap-2">
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2 ml-5  hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className=" w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%B8%E0%A7%8D%E0%A6%AC%E0%A6%BE%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A7%8D%E0%A6%AF-%E0%A6%B8%E0%A7%81%E0%A6%B0%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%BE-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-8524.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2  ml-5  hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2  ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2 ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2  ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2  ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2 ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2  ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2  ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[232px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[189px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%97%E0%A7%83%E0%A6%B9%E0%A6%B8%E0%A7%8D%E0%A6%A5%E0%A6%BE%E0%A6%B2%E0%A7%80-%E0%A6%AA%E0%A6%A3%E0%A7%8D%E0%A6%AF-4429.jpg" alt="" />
-        <p className="text-[#5fadf1] py-2  ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[374px] hover:shadow-sm  bg-[#ffffff]">
-        <img className="w-full lg:h-[316px]" src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%A5%E0%A7%8D%E0%A6%B0%E0%A7%80-%E0%A6%AA%E0%A6%BF%E0%A6%9B-2415.jpg" alt="" />
-        <p className="text-[#5fadf1] py-3 ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-      {/* card */}
-      <div className="lg:h-[374px] hover:shadow-sm bg-[#ffffff]">
-        <img className="w-full lg:h-[316px] " src="https://new2.codexdigitalagency.com/uploads/category/%E0%A6%A5%E0%A7%8D%E0%A6%B0%E0%A7%80-%E0%A6%AA%E0%A6%BF%E0%A6%9B-2415.jpg" alt="" />
-        <p className="text-[#5fadf1] py-3 ml-5 hover:text-orange-600">গৃহস্থালী পণ্য</p>
-      </div>
-    </div>
-  </div>;
+  return <div>
+       <h2>Order Details page</h2>
+  </div>
 };
 
 export default Root;
